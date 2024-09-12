30e56746b3fe1695973d01744454d112b6d3a087
solomon sunday <schinoyerem007@gmail.com>
2024-09-12 12:19:08 +0100

    using module to breakdown the app to smaller components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
-// import { NgModule } from "@angular/core";
-// import { AppComponent } from "./app.component";
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
 
-// @NgModule({
-//   declarations: [AppComponent],
-
-// })
-// export class AppModule
-// {
-
-// }
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, UserComponent],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, FormsModule, SharedModule, TasksModule],
+})
+export class AppModule {}
--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,15 +1,11 @@
 import { Component, inject, Input } from '@angular/core';
 import { Task } from './task.model';
-import { CardComponent } from '../../shared/card/card.component';
-import { DatePipe } from '@angular/common';
 import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-task',
-  standalone: true,
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
-  imports: [CardComponent, DatePipe],
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
